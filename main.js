d88cfffe59801b28efbafc2ce705be4c46784379
saudali195 <160694354+saudali195@users.noreply.github.com>
2024-06-07 02:18:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// let name1 = "Vairiable by Let!"
+// console.log(name1 );
+// name1 = "Saud"
+// console.log(name1);
+// var name1 = "Variable by var"
+// name1 = "Saud"
+// console.log(name1);
+var name1 = "Variable by Const";
+console.log(name1);
---
+++ main.ts
@@ -0,0 +1,19 @@
+// let name1 = "Vairiable by Let!"
+// console.log(name1 );
+// name1 = "Saud"
+// console.log(name1);
+
+// var name1 = "Variable by var"
+// name1 = "Saud"
+// console.log(name1);
+
+let name1 = "Saud" //infering type
+let name2:string = "Saud" //strongly type
+// name2 = 1000 errors
+let num2 = 1000   //infering typee
+let num3:number = 1000 //strongly data Typed
+let news = false //infering type
+let news1: boolean = true
+let anyData: any = 1000  // not recomended
+anyData = "Hello, World!"
+anyData = true
